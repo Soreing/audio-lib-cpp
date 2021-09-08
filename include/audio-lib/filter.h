5d3ee90d01276b12295a250a03701283b6b40107
Soreing <Soreing.dev@gmail.com>
2021-09-08 17:44:21 +0400

    Expanded FIR Filter constructors

--- include/audio-lib/filter.h
+++ include/audio-lib/filter.h
@@ -10,7 +10,12 @@ public:
 	size_t size;
 
 public:
+	FIRFilter_i64();
 	FIRFilter_i64(int taps, int stop_freq, int sample_freq);
+	FIRFilter_i64(const FIRFilter_i64 &other);
+	~FIRFilter_i64();
+
+	void init(int taps, int stop_freq, int sample_freq);
 };
 
 #endif
--- src/filter.cpp
+++ src/filter.cpp
@@ -1,21 +1,51 @@
-#include "math.h"
 #include <audio-lib/filter.h>
+#include <string.h>
+#include <math.h>
 
 #define MATH_PI 3.1415f
 #define SINC(fc, n) sin((2 * MATH_PI * fc * n)) / (MATH_PI * n)
 #define HAMM(N, n)  0.54f + 0.46f * cos(n * 2 * MATH_PI / N)
 
-FIRFilter_i64::FIRFilter_i64(int taps, int stop_freq, int sample_freq)
+FIRFilter_i64::FIRFilter_i64() :
+	coefs(NULL), size(0)
 {
+}
+
+FIRFilter_i64::FIRFilter_i64(int taps, int stop_freq, int sample_freq):
+	coefs(NULL), size(0)
+{	
+	init(taps, stop_freq, sample_freq);
+}
+
+FIRFilter_i64::FIRFilter_i64(const FIRFilter_i64 &other):
+	coefs(new llong[other.size]), size(other.size)
+
+{	
+	memcpy(coefs, other.coefs, sizeof(llong) * size);
+}
+
+FIRFilter_i64::~FIRFilter_i64()
+{
+	if(coefs != NULL)
+	{	delete[] coefs;
+	}
+}
+
+void FIRFilter_i64::init(int taps, int stop_freq, int sample_freq)
+{
+	if(coefs != NULL)
+	{	delete[] coefs;
+	}
+	
 	size = taps;
 	coefs = new llong[size];
 
 	double ratio = (float)stop_freq / (float)sample_freq;
 	double f_coef;
 
-	for (size_t i = 0, p = i - size / 2; i < size; i++, p++)
+	for (int i = 0, p = i - size / 2; i < (int)size; i++, p++)
 	{
-		f_coef = SINC(ratio, p);
+		f_coef  = SINC(ratio, p);
 		f_coef *= HAMM(size, p);
 		coefs[i] = (llong)(f_coef * 4294967296);
 	}
